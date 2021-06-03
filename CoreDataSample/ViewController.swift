3090741a95ef2dc1ee16b6059f820ae6c1ab02ea
rinetrous <rinellie1228@icloud.com>
2021-06-03 16:11:47 +0900

    Initial Commit

---
+++ CoreDataSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataSample
+//
+//  Created by 野田凜太郎 on 2021/06/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
