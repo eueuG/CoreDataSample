9c4f93efb353be91469d80566d07b1fb51a26e5a
rinetrous <rinellie1228@icloud.com>
2021-06-03 16:27:09 +0900

    rootVCに永続化コンテナを設定（紐付け）

--- CoreDataSample/SceneDelegate.swift
+++ CoreDataSample/SceneDelegate.swift
@@ -17,6 +17,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
+        
+        // MARK: - CoreDataSetting
+        let window = UIWindow(windowScene: scene as! UIWindowScene)
+        self.window = window
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let rootVC = storyboard.instantiateViewController(identifier: "ViewController") as? ViewController else {
+            return
+        }
+        
+        //ルートビューコントローラーに永続化コンテナを設定する。
+        rootVC.container = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+        
+        self.window?.rootViewController = rootVC
+        self.window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
